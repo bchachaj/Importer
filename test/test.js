1bba1ec66909c1052247c6109c6d9e982ba10900
Ben <benedict.chachaj@student.fairfield.edu>
2019-09-16 11:57:40 -0400

    v1.1

--- test/test.js
+++ test/test.js
@@ -1,22 +1,28 @@
 const fs = require("fs");
 const parseHTML = require('../lib/utils/parseHTML');
-// path.join(__dirname, '/start.html')
 const path = require('path');
 const cheerio = require('cheerio');
 
 const html = fs.readFileSync(path.join(__dirname, '/recipe.html'));
 const url = "https://cooking.nytimes.com/recipes/1017518-panzanella-with-mozzarella-and-herbs";
-const { attemptExtractByProps } = require('./../lib/utils/extractMeta');
+const {
+    attemptExtractByProps
+} = require('./../lib/utils/extractMeta');
+const {
+    buildResponseObject
+} = require('./../lib/utils/buildResponseObject');
+
 const recipe = require('./recipe.json');
 
 
 const assert = require('assert');
 
-describe('hooks', function () {
-    let extracted, attemptedProps;
+describe('Importer', function () {
+    let extracted, attemptedProps, jsonRes;
     before(async function () {
         extracted = await parseHTML(html, url);
         attemptedProps = attemptExtractByProps(cheerio.load(html))
+        jsonRes = buildResponseObject(attemptedProps);
     });
 
     describe('parseHTML - extract props', async function () {
@@ -34,14 +40,32 @@ describe('hooks', function () {
             assert.equal(extracted.ingredients.length, recipe.ingredients.length);
 
         });
+
         it('correctly returns recipe direction data', async function () {
             const extracted = await parseHTML(html, url);
+
             assert.equal(extracted.directions[0].length, recipe.steps.join(" ").length);
 
         });
 
     });
 
+    describe('Properly formats response', async function () {
+
+        it('populates ingredients', function () {
+            assert.notEqual(jsonRes.ingredients.length, 0)
+        });
+
+        it('populates steps', function () {
+            assert.notEqual(jsonRes.steps.length, 0)
+        });
+
+        it('categorizes ingredient fields', function () {
+            assert.deepEqual(jsonRes.ingredients[0], recipe.ingredients[0])
+        });
+
+    });
+
+
+
 });
-
-
